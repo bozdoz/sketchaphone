ae354e25a0c9da16020e9f0d9286852d0ac247ab
bozdoz <howaboutben@gmail.com>
2019-09-20 20:32:12 -0300

    try now build now

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,13 +6,6 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
@@ -24,9 +17,8 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    'gatsby-plugin-styled-components'
+    'gatsby-plugin-styled-components',
   ],
 }
